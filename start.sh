29deccc88b44ce2e6f37145a9af2213825c831a4
n <2585803+tt5@users.noreply.github.com>
2024-01-06 18:04:04 +0100

    update, podman

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-civo kubernetes create -r "Traefik-v2-nodeport" -w -e "default-default" -n 1 -s "g4s.kube.large"
+civo kubernetes create -r "Traefik-v2-nodeport" -r "metrics-server" -w -e "default-default" -n 1 -s "g4s.kube.large"
 
 NAME=$(civo kubernetes list -o json | jq -r '.[].name')
 civo kubernetes config ${NAME} -s
+kubectl create namespace dev
